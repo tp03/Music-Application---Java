8c4542d2d6b5a801455795335717d0a4a025d7c7
Marcin Połosak <marcin.polosak@wp.pl>
2024-04-14 21:44:15 +0200

    Visual changes for login/register windows

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -9,11 +9,12 @@ public class LoginFrame extends JFrame {// inheriting JFrame
     JPanel mainPanel = new JPanel();
     private JPanel loginPanel;
     private JPanel registerPanel;
-    private JButton loginbutton;
+    private JButton move_to_register;
     public JButton registerbutton;
 
     LoginFrame() {
-        this.setSize(400, 300);
+        this.setSize(600, 800);
+        this.setLocationRelativeTo(null);
 
         this.loginPanel = create_login_panel();
         this.registerPanel = create_register_panel();
@@ -32,49 +33,61 @@ public class LoginFrame extends JFrame {// inheriting JFrame
         JPanel login_panel = new JPanel();
         login_panel.setBorder(new EmptyBorder(10, 10, 10, 10));
         login_panel.setLayout(new GridBagLayout());
-        center_elements(login_panel);
+        center_elements_login(login_panel);
 
-        login_panel.setSize(400, 500);
-        switch_scane_button(this.loginbutton, "register", "register_panel");
+        login_panel.setSize(600, 800);
+        // login_panel.setBackground(new Color(0, 0, 0));
+        switch_scane_button(this.move_to_register, "register", "register_panel");
 
         return login_panel;
 
     }
 
-    void center_elements(JPanel login_panel) {
+    GridBagConstraints create_gbc() {
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.anchor = GridBagConstraints.NORTH;
         gbc.anchor = GridBagConstraints.CENTER;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(10, 10, 10, 10);
-        JLabel l1, l2;
-        l1 = new JLabel("First Label.");
-        l1.setBounds(50, 50, 100, 30);
-        this.loginbutton = new JButton("click");// create button
+        gbc.weighty = 1;
+        return gbc;
+    }
 
+    void center_elements_login(JPanel login_panel) {
+        GridBagConstraints gbc = create_gbc();
+
+        // Labels
+        JLabel l1, l2;
+        l1 = new JLabel("Login:");
+        l1.setBounds(50, 50, 100, 30);
+        l2 = new JLabel("Password:");
+        l2.setBounds(50, 50, 100, 30);
+
+        // InputFields
         JTextField login_input = new JTextField();
         JPasswordField passwordField = new JPasswordField();
-        // login_input.setBounds(50, 100, 150, 20);
-        // passwordField.setSize(150, 20);
-        passwordField.setPreferredSize(new Dimension(150, 30));
-        // this.loginbutton.setPreferredSize(new Dimension(150, 30));
-        JPanel buttons = new JPanel(new GridBagLayout());
-        // buttons.setPreferredSize(new Dimension(20, 300));
-        JButton start = new JButton("Start");
-        start.setPreferredSize(new Dimension(400, 20));
-        buttons.add(start, gbc);
+        JButton loginbutton = new JButton("Login");
+        this.move_to_register = new JButton("Register");
 
-        // buttons.add(Box.createRigidArea(new Dimension(0, 10)), gbc);
-        JButton input_login = new JButton("Start");
-        input_login.setSize(new Dimension(200, 20));
+        // Configs
+        passwordField.setPreferredSize(new Dimension(250, 20));
 
-        buttons.add(input_login, gbc);
-        buttons.add(new JButton("Help"), gbc);
-        buttons.add(loginbutton, gbc);
+        JPanel main_objts = new JPanel(new GridBagLayout());
 
-        gbc.weighty = 1;
-        login_panel.add(buttons, gbc);
+        main_objts.add(l1, gbc);
+        main_objts.add(login_input, gbc);
+        main_objts.add(l2, gbc);
+        main_objts.add(passwordField, gbc);
+
+        gbc.insets = new Insets(30, 10, 5, 10);
+        main_objts.add(loginbutton, gbc);
+        gbc.insets = new Insets(0, 10, 30, 10);
+        main_objts.add(move_to_register, gbc);
+        main_objts.setSize(300, 600);
+        // main_objts.setBackground(new Color(0, 0, 0));
+
+        login_panel.add(main_objts, gbc);
 
     }
 
@@ -88,24 +101,49 @@ public class LoginFrame extends JFrame {// inheriting JFrame
     }
 
     JPanel create_register_panel() {
-        JPanel register_panel = new JPanel(new GridLayout(1, 1));
-        JLabel l1, l2;
-        l1 = new JLabel("First Label.");
-        l1.setBounds(50, 50, 100, 30);
-        this.registerbutton = new JButton("click2");// create button
+        JPanel register_panel = new JPanel();
+        register_panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        register_panel.setLayout(new GridBagLayout());
+        center_elements_register(register_panel);
 
-        JTextField login_input = new JTextField();
-        JPasswordField passwordField = new JPasswordField();
-        login_input.setBounds(50, 100, 150, 20);
-        passwordField.setBounds(50, 150, 150, 20);
-        this.registerbutton.setBounds(50, 170, 100, 40);
-        register_panel.add(login_input);
-        register_panel.add(this.registerbutton);// adding button on frame
-        register_panel.add(l1);
-        register_panel.add(passwordField);
-        register_panel.setLayout(null);
         register_panel.setSize(400, 500);
         return register_panel;
+
+    }
+
+    void center_elements_register(JPanel register_panel) {
+        GridBagConstraints gbc = create_gbc();
+
+        // Labels
+        JLabel l1, l2, l3;
+        l1 = new JLabel("Username:");
+        l1.setBounds(50, 50, 100, 30);
+        l2 = new JLabel("Login:");
+        l2.setBounds(50, 50, 100, 30);
+        l3 = new JLabel("Password:");
+        l3.setBounds(50, 50, 100, 30);
+
+        // InputFields
+        JTextField username_input = new JTextField();
+        JTextField login_input = new JTextField();
+        JPasswordField passwordField = new JPasswordField();
+        this.registerbutton = new JButton("Create user");
+
+        // Configs
+        passwordField.setPreferredSize(new Dimension(250, 20));
+
+        JPanel main_objts = new JPanel(new GridBagLayout());
+
+        main_objts.add(l1, gbc);
+        main_objts.add(username_input, gbc);
+        main_objts.add(l2, gbc);
+        main_objts.add(login_input, gbc);
+        main_objts.add(l3, gbc);
+        main_objts.add(passwordField, gbc);
+        gbc.insets = new Insets(30, 10, 30, 10);
+        main_objts.add(this.registerbutton, gbc);
+
+        register_panel.add(main_objts, gbc);
 
     }
 
