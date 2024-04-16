916b481ed2cf253015e27cd5dd78532fbb128206
Adrian <akazmie1>
2024-04-16 15:33:33 +0200

    zapisywanie playlisty do pliku playlist.txt

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Playlist {
@@ -7,4 +10,34 @@ public class Playlist {
         this.songs = new ArrayList<>();
     }
 
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    public void savePlaylistToFile(String playlist_name) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(playlist_name))) {
+            for (Song song : songs) {
+                String line = song.getName() + " - " + song.getAuthor();
+                writer.write(line);
+                writer.newLine();
+            }
+            System.out.println("Song saved to " + playlist_name);
+        } catch (IOException e) {
+            System.err.println("Error occurred while saving playlist: " + e.getMessage());
+        }
+    }
+
+    private String formatSong(Song song) {
+        return song.getName() + " - " + song.getAuthor();
+    }
+
+    public static void main(String[] args) {
+        // Przykładowe użycie
+        Playlist myPlaylist = new Playlist();
+        Song song1 = new Song("Pila tango", "Bajm", "image1.jpg", "recording1.mp3");
+        Song song2 = new Song("Song", "Author", "image2.jpg", "recording2.mp3");
+        myPlaylist.addSong(song1);
+        myPlaylist.addSong(song2);
+        myPlaylist.savePlaylistToFile("playlist.txt");
+    }
 }
