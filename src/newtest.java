400fceacedaf27e97630ae501aaf2b97ebf70d9d
tzalews1 <zalewski.tomek03@gmail.com>
2024-05-28 23:40:50 +0200

    tests?

---
+++ src/newtest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class newtest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
+
