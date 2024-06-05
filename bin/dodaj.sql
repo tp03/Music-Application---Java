6bf90fbe918e9032008f02b662d5577e1f0c2262
akowals2 <01178230@pw.edu.pl>
2024-06-05 22:34:57 +0200

    added views to song panels

--- bin/dodaj.sql
+++ bin/dodaj.sql
@@ -8,14 +8,6 @@ SELECT * FROM SONG_DATA;
 
 INSERT INTO author VALUES (2, 'Zenon Martyniuk', 99999);
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-INSERT INTO SONG_DATA VALUES (1, 'narazie_nic', 'recordings/chwile.mp3', 'assets/zenek1.png');
-INSERT INTO SONG_DATA VALUES (2, 'narazie_nic', 'recordings/gwiazda.mp3', 'assets/zenek2.jpg');
-INSERT INTO SONG_DATA VALUES (3, 'narazie_nic', 'recordings/gwiazda.mp3', 'assets/zenek3.jpeg');
-=======
->>>>>>> Anton
 INSERT INTO author VALUES (3, 'Strachy na Lachy', 120000);
 
 INSERT INTO author VALUES (4, 'Sławomir', 100000);
@@ -30,10 +22,6 @@ INSERT INTO SONG_DATA VALUES (5, 'narazie_nic', 'recordings/pila-tango.mp3', 'as
 INSERT INTO SONG_DATA VALUES (6, 'narazie_nic', 'recordings/slawomir.mp3', 'assets/slawomir.jpg');
 INSERT INTO SONG_DATA VALUES (7, 'narazie_nic', 'recordings/przechyly.mp3', 'assets/szanta.png');
 
-<<<<<<< HEAD
-=======
->>>>>>> origin/Marcin
->>>>>>> Anton
 
 INSERT INTO SONG VALUES (1, 'Życie To Są Chwilę', 200, 1, 10000);
 INSERT INTO SONG VALUES (2, 'Przez Twe Oczy Zielone', 213, 2, 100010);
